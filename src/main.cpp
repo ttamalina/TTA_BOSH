c07aa731b7e2587fbb47dbd931bd4f09a9d7e7f9
ttamalina42 <ttamalina42@protonmail.com>
2020-01-10 20:14:48 +0200

    remote debugmode in progress

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,76 @@
 
 using namespace std;
 
+#include <SFML/Network.h>
+#include <unistd.h>
+
+#ifdef DEBUG
+    sfUdpSocket* udpSock;
+    sfTcpListener* tcpList;
+    sfTcpSocket* tcpSock;
+    sfPacket* packet;
+    sfIpAddress localAddr;
+    void debug_setup()
+    {
+        printf("Run in debug mode.\n");
+        udpSock = sfUdpSocket_create();
+        packet = sfPacket_create();
+        tcpSock = sfTcpSocket_create();
+        tcpList = sfTcpListener_create();
+        localAddr = sfIpAddress_getLocalAddress();
+        sfUdpSocket_bind(udpSock, 6969, sfIpAddress_Broadcast);
+        sfTcpListener_listen(tcpList, 0, localAddr);
+        sfPacket_writeUint32(packet, sfTcpListener_getLocalPort(tcpList));
+        sfPacket_writeString(packet, localAddr.address);
+        sfUdpSocket_sendPacket(udpSock, packet, sfIpAddress_Broadcast, 6969);
+        sfTcpListener_accept(tcpList, &tcpSock);
+        printf("Accepted from server: %s, port: %hu\n", sfTcpSocket_getRemoteAddress(tcpSock).address, sfTcpSocket_getRemotePort(tcpSock));
+    }
+    void debug_send_frame(RGBImage frame)
+    {
+        sfPacket_clear(packet);
+        sfPacket_writeUint32(packet, frame.width);
+        sfPacket_writeUint32(packet, frame.height);
+        sfPacket_append(packet, frame.data, frame.size);
+        sfUdpSocket_sendPacket(udpSock, packet, sfIpAddress_Broadcast, 6969);
+    }
+    void debug_destroy()
+    {
+
+    }
+#endif
+
+
 int main(int argc, char** argv)
 {
     const int screenWidth = XRES;
     const int screenHeight = YRES;
     const RGBImage *frame_ptr = nullptr;
     Webcam webcam("/dev/video0", XRES, YRES);
-    InitWindow(screenWidth, screenHeight, "Camera monitor");
-    SetTargetFPS(60);    
-    while (!WindowShouldClose())
+
+#ifdef DEBUG
+    debug_setup();
+#endif
+
+    //InitWindow(screenWidth, screenHeight, "Camera monitor");
+    //SetTargetFPS(60);  
+    while(1)
     {
         frame_ptr = &webcam.frame();
         Frame frame(*frame_ptr);
+    }
+
+    printf("Press any button to exit.\n");
+    getchar();
+
+#ifdef DEBUG
+    debug_destroy();
+#endif
+
+    /*  
+
+    while (!WindowShouldClose())
+    {
         if(IsKeyDown(KEY_S))
         {
             makeScreenshot(frame_ptr, "frame.ppm");
@@ -35,5 +93,6 @@ int main(int argc, char** argv)
     }
 
     CloseWindow(); 
+    */
     return 0;
 }
